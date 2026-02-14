f40a639eaea4608786e9283c28b8c148fec60201
Chris Mitchelmore <chrismitchelmore@gmail.com>
2026-02-14 08:38:39 +0000

    fix: [mac][ios] enable Deepgram numerals and fix iOS intent import

--- Sources/SpeakApp/DeepgramTranscriptionProvider.swift
+++ Sources/SpeakApp/DeepgramTranscriptionProvider.swift
@@ -54,6 +54,7 @@ final class DeepgramLiveTranscriber: @unchecked Sendable {
             URLQueryItem(name: "model", value: model),
             URLQueryItem(name: "punctuate", value: "true"),
             URLQueryItem(name: "smart_format", value: "true"),
+            URLQueryItem(name: "numerals", value: "true"),
             URLQueryItem(name: "interim_results", value: "true"),
             URLQueryItem(name: "encoding", value: "linear16"),
             URLQueryItem(name: "sample_rate", value: String(sampleRate)),
@@ -278,6 +279,7 @@ struct DeepgramTranscriptionProvider: TranscriptionProvider {
         var queryItems = [
             URLQueryItem(name: "model", value: extractModelName(from: model)),
             URLQueryItem(name: "punctuate", value: "true"),
+            URLQueryItem(name: "numerals", value: "true"),
             URLQueryItem(name: "utterances", value: "true")
         ]
 
